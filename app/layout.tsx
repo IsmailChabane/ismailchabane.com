028d3d9ca3fc30452bde63e396f78ecb399d4617
Ismail Chabane <75178106+Hx-Regex@users.noreply.github.com>
2025-10-17 15:35:00 +0100

    feat: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
-import { ThemeProvider } from "@/providers/theme-provider"
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 
 const geistSans = Geist({
@@ -111,6 +112,7 @@ export default function RootLayout({
             </main>
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
