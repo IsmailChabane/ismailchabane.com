36645e07df536d9b7b1abd48abdda4d6375a6284
Ismail Chabane <75178106+Hx-Regex@users.noreply.github.com>
2025-10-20 14:26:26 +0100

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,54 +3,27 @@ module.exports = {
     siteUrl: 'https://ismailchabane.com',
     generateRobotsTxt: true,
     sitemapSize: 5000,
-    /*
-        Additional paths
-        NOTE : TO BE CHANGED LATER TO BE AUTO GENERATED FROM THE PROJECTS DATA
-    */
     additionalPaths: async () => ([
-      {
-        loc: '/',
-        images: [
-          'https://ismailchabane.com/assets/ismailchabane.png',
-        ],
-      },
-      {
-        loc: '/projects/africkana',
-        images: [
+      { loc: 'https://ismailchabane.com/', images: ['https://ismailchabane.com/assets/ismailchabane.png'] },
+      { loc: 'https://ismailchabane.com/projects/africkana', images: [
           'https://ismailchabane.com/work/africkana/website.png',
           'https://ismailchabane.com/work/africkana/website2.png',
-        ],
-      },
-      {
-        loc: '/projects/kora-awards',
-        images: [
+      ]},
+      { loc: 'https://ismailchabane.com/projects/kora-awards', images: [
           'https://ismailchabane.com/work/koraawards/website.png',
-        ],
-      },
-      {
-        loc: '/projects/sofimed-maroc',
-        images: [
+      ]},
+      { loc: 'https://ismailchabane.com/projects/sofimed-maroc', images: [
           'https://ismailchabane.com/work/sofimedmaroc/website.png',
-        ],
-      },
-      {
-        loc: '/projects/softskills-club',
-        images: [
+      ]},
+      { loc: 'https://ismailchabane.com/projects/softskills-club', images: [
           'https://ismailchabane.com/work/softskillsclub/website.png',
-        ],
-      },
-      {
-        loc: '/projects/vote-moi',
-        images: [
+      ]},
+      { loc: 'https://ismailchabane.com/projects/vote-moi', images: [
           'https://ismailchabane.com/work/votemoi/website.png',
           'https://ismailchabane.com/work/votemoi/website2.png',
-        ],
-      },
-      {
-        loc: '/projects/pdf-orca',
-        images: [
+      ]},
+      { loc: 'https://ismailchabane.com/projects/pdf-orca', images: [
           'https://ismailchabane.com/work/pdforca/website.png',
-        ],
-      },
+      ]},
     ]),
   };
