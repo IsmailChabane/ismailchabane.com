625b1c0ae88b4b68336a1d1f3b7adf7c1f8c3223
Ismail Chabane <75178106+Hx-Regex@users.noreply.github.com>
2025-10-20 14:06:17 +0100

    fix(sitemap) : test

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,35 +1,32 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://ismailchabane.com',
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
-  // Return plain field objects; next-sitemap will handle XML generation
-  additionalPaths: async () => {
-    const extras = [
-      {
-        loc: '/',
-        images: [
-          {
-            url: 'https://ismailchabane.com/assets/ismailchabane.png',
-            caption: 'Ismail Chabane Profile Photo',
-          },
-        ],
-      },
-    ];
-
-    const projectImages = require('./lib/seo/projects-images.json');
-    for (const p of projectImages) {
-      extras.push({
-        loc: `/projects/${p.id}`,
-        images: p.images.map((img) => ({
-          url: `https://ismailchabane.com${img.src}`,
-          caption: img.alt,
-        })),
-      });
-    }
-
-    return extras;
-  },
-};
-
-
+    siteUrl: 'https://ismailchabane.com',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    additionalPaths: async () => {
+      const extras = [
+        {
+          loc: '/',
+          images: [
+            {
+              loc: 'https://ismailchabane.com/assets/ismailchabane.png',
+              title: 'Ismail Chabane Profile Photo',
+            },
+          ],
+        },
+      ];
+  
+      const projectImages = require('./lib/seo/projects-images.json');
+      for (const p of projectImages) {
+        extras.push({
+          loc: `/projects/${p.id}`,
+          images: (p.images || []).map((img) => ({
+            loc: `https://ismailchabane.com${img.src}`,
+            title: img.alt,
+          })),
+        });
+      }
+  
+      return extras;
+    },
+  };
