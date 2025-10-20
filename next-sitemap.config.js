d4ae1068a6dafd7ac987dc8d7976ff62f40fd99d
Ismail Chabane <75178106+Hx-Regex@users.noreply.github.com>
2025-10-20 14:32:36 +0100

    fix(sitemap)  : remove iamges until further test

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,26 +4,12 @@ module.exports = {
     generateRobotsTxt: true,
     sitemapSize: 5000,
     additionalPaths: async () => ([
-      { loc: 'https://ismailchabane.com/', images: ['https://ismailchabane.com/assets/ismailchabane.png'] },
-      { loc: 'https://ismailchabane.com/projects/africkana', images: [
-          'https://ismailchabane.com/work/africkana/website.png',
-          'https://ismailchabane.com/work/africkana/website2.png',
-      ]},
-      { loc: 'https://ismailchabane.com/projects/kora-awards', images: [
-          'https://ismailchabane.com/work/koraawards/website.png',
-      ]},
-      { loc: 'https://ismailchabane.com/projects/sofimed-maroc', images: [
-          'https://ismailchabane.com/work/sofimedmaroc/website.png',
-      ]},
-      { loc: 'https://ismailchabane.com/projects/softskills-club', images: [
-          'https://ismailchabane.com/work/softskillsclub/website.png',
-      ]},
-      { loc: 'https://ismailchabane.com/projects/vote-moi', images: [
-          'https://ismailchabane.com/work/votemoi/website.png',
-          'https://ismailchabane.com/work/votemoi/website2.png',
-      ]},
-      { loc: 'https://ismailchabane.com/projects/pdf-orca', images: [
-          'https://ismailchabane.com/work/pdforca/website.png',
-      ]},
+      { loc: 'https://ismailchabane.com/' },
+      { loc: 'https://ismailchabane.com/projects/africkana' },
+      { loc: 'https://ismailchabane.com/projects/kora-awards' },
+      { loc: 'https://ismailchabane.com/projects/sofimed-maroc' },
+      { loc: 'https://ismailchabane.com/projects/softskills-club' },
+      { loc: 'https://ismailchabane.com/projects/vote-moi' },
+      { loc: 'https://ismailchabane.com/projects/pdf-orca' },
     ]),
   };
